1ddf711fe3100f99e0d29f97331c92b18d3e4897
CESCO <cescovalledev@gmail.com>
2024-01-11 22:22:51 -0600

    trying homework

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -5,8 +5,33 @@ interface Props {
   label: string;
   /**Label Size */
   size?: "normal" | "h1" | "h2" | "h3";
+  /**If wants to be all caps */
+  allCaps?: boolean;
+  /**Predefined colors for text */
+  color?: "text-primary" | "text-secondary" | "text-tertiary";
+  /**Hexadecimal code for text color */
+  fontColor?: string;
 }
 
-export const MyLabel = ({ label, size = "normal" }: Props) => {
-  return <span className={size}>{label}</span>;
+export const MyLabel = ({
+  label,
+  size = "normal",
+  allCaps = false,
+  color,
+  fontColor,
+}: Props) => {
+  return (
+    <span
+      className={size + color ? color : ""}
+      style={
+        fontColor
+          ? {
+              color: fontColor,
+            }
+          : {}
+      }
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
 };
